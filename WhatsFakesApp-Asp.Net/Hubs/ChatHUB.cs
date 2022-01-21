d0fd9b4abbd91a4a8bb11a17ab0fa5c9f33eef97
Nazaralieff <67953949+Nazaralieff@users.noreply.github.com>
2022-01-21 21:11:21 +0400

    Chat Succesfully working

--- WhatsFakesApp-Asp.Net/Hubs/ChatHUB.cs
+++ WhatsFakesApp-Asp.Net/Hubs/ChatHUB.cs
@@ -5,9 +5,9 @@ namespace WhatsFakesApp_Asp.Net.Hubs
 {
     public class ChatHUB:Hub
     {
-        public async Task SendMessage(string user, string message)
+        public async Task SendMessage(string message)
         {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            await Clients.All.SendAsync("ReceiveMessage", message);
         }
     }
 }
--- WhatsFakesApp-Asp.Net/wwwroot/js/chat.js
+++ WhatsFakesApp-Asp.Net/wwwroot/js/chat.js
@@ -6,8 +6,26 @@ var connection = new signalR.HubConnectionBuilder().withUrl("/chat").build();
 //document.getElementById("sendButton").disabled = /*true*/;
 
 connection.on("ReceiveMessage", function (user, message) {
+    console.log("message received")
+
+    var ul = document.createElement("ul");
+    ul.classList = "receiver msg";
+
     var li = document.createElement("li");
-    document.getElementById("messagesList").appendChild(li);
+
+    var p = document.createElement("p");
+    p.innerHTML = message;
+
+    var span = document.createElement("ul");
+    span.innerHTML = "12:02";
+
+    li.appendChild(p);
+    li.appendChild(span);
+    ul.appendChild(li);
+
+    console.log(ul);
+
+    document.getElementById("chat-messages-container").appendChild(ul);
     // We can assign user-supplied strings to an element's textContent because it
     // is not interpreted as markup. If you're assigning in any other way, you 
     // should be aware of possible script injection concerns.
@@ -20,11 +38,13 @@ connection.start().then(function () {
     return console.error(err.toString());
 });
 
-document.getElementById("sendButton").addEventListener("click", function (event) {
-    var user = document.getElementById("userInput").value;
-    var message = document.getElementById("messageInput").value;
-    connection.invoke("SendMessage", user, message).catch(function (err) {
+document.getElementById("send-message-btn").addEventListener("click", function (event) {
+    var message = document.getElementById("message-input").value;
+    console.log("message : " + message);
+    //var message = document.getElementById("messageInput").value;
+    connection.invoke("SendMessage", message).catch(function (err) {
         return console.error(err.toString());
     });
+    console.log("message sended")
     event.preventDefault();
 });

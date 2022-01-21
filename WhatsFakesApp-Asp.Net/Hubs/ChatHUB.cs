8827f334172282cc298357c0bfb096d2815ffdf4
Nazaralieff <67953949+Nazaralieff@users.noreply.github.com>
2022-01-21 17:28:56 +0400

    First version of App and Successfully working for All Users

---
+++ WhatsFakesApp-Asp.Net/Hubs/ChatHUB.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace WhatsFakesApp_Asp.Net.Hubs
+{
+    public class ChatHUB:Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
---
+++ WhatsFakesApp-Asp.Net/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WhatsFakesApp_Asp.Net.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

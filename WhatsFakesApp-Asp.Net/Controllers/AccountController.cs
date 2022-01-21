280c3df7efe1511572026b7617257f13a51bfc20
Nazaralieff <67953949+Nazaralieff@users.noreply.github.com>
2022-01-21 22:23:24 +0400

    SignIn Successfully Working

--- WhatsFakesApp-Asp.Net/Controllers/AccountController.cs
+++ WhatsFakesApp-Asp.Net/Controllers/AccountController.cs
@@ -1,12 +1,47 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using WhatsFakesApp_Asp.Net.Data;
+using WhatsFakesApp_Asp.Net.Models;
+using WhatsFakesApp_Asp.Net.ViewModel;
 
 namespace WhatsFakesApp_Asp.Net.Controllers
 {
 	public class AccountController : Controller
 	{
+		private readonly AppDbContext _context;
+		private readonly UserManager<CustomUser> _userManager;
+		private readonly SignInManager<CustomUser> _signInManager;
+
+		public AccountController(AppDbContext context, UserManager<CustomUser> userManager, SignInManager<CustomUser> signInManager)
+		{
+			_context = context;
+			_userManager = userManager;
+			_signInManager = signInManager;
+		}
 		public IActionResult Login()
 		{
 			return View();
+		}
+
+
+		[HttpPost]
+		public async Task<IActionResult> Login(VmLogin model)
+		{
+			if (!ModelState.IsValid)
+			{
+				return View(model);
+			}
+
+			var result = await _signInManager.PasswordSignInAsync(model.Mail, model.Password, false, false);
+
+			if (!result.Succeeded)
+			{
+				ModelState.AddModelError("", "Login or Password is not correct");
+				return View(model);
+			}
+
+			return RedirectToAction("Index", "Home");
 		}
 
 
@@ -14,6 +49,27 @@ namespace WhatsFakesApp_Asp.Net.Controllers
 
 		public IActionResult Register()
 		{
+			return View();
+		}
+
+
+		[HttpPost]
+		public async Task<IActionResult> Register(VmRegister model)
+		{
+			if(!ModelState.IsValid) return View(model);
+			CustomUser user = new CustomUser()
+			{
+				Fullname = model.Fullname,
+				Email = model.Mail,
+				UserName = model.Mail,
+			};
+
+			var result = await _userManager.CreateAsync(user, model.Password);
+			if (result.Succeeded)
+			{
+				await _signInManager.SignInAsync(user, false);
+				return RedirectToAction("Index", "Home");
+			}
 			return View();
 		}
 	}
--- WhatsFakesApp-Asp.Net/wwwroot/assets/js/script.js
+++ WhatsFakesApp-Asp.Net/wwwroot/assets/js/script.js
@@ -1,56 +1,73 @@
 $(document).ready(() => {
-  Adjust();
-  $(window).on("resize", () => {
     Adjust();
-  });
+    //inputFocus();
+
+    $(window).on("resize", () => {
+        Adjust();
+    });
 
 
 
     var inputs = document.getElementsByClassName("my-form-input");
-  var labels = document.getElementsByClassName("my-form-label");
-  for(let i =0; i<=inputs.length; i++){
-    $(inputs[i]).focus(()=>{
-      for(let j = 0; j <= labels.length; j++){
-        if(inputs[i].dataset.index == labels[j].dataset.index){
-          $(labels[j]).css({
-            "bottom" : "35px",
-            "font-size" : "12px"
-          })
-        }
-      }
-    })
-  }
+    var labels = document.getElementsByClassName("my-form-label");
+    for (let i = 0; i <= inputs.length; i++) {
+        $(inputs[i]).focus(() => {
+            for (let j = 0; j <= labels.length; j++) {
+                if (inputs[i].dataset.index == labels[j].dataset.index) {
+                    $(labels[j]).css({
+                        "bottom": "35px",
+                        "font-size": "12px"
+                    })
+                }
+            }
+        })
+    }
 
-  for(let i =0; i<=inputs.length; i++){
-    $(inputs[i]).blur(()=>{
-      for(let j = 0; j <= labels.length; j++){
-        if(inputs[i].dataset.index == labels[j].dataset.index){
-          if(inputs[i].value == ""){
-            $(labels[j]).css({
-              "bottom" : "5px",
-              "font-size" : "14px"
-            })
-          }
-        }
-      }
-    })
-  }
+    for (let i = 0; i <= inputs.length; i++) {
+        $(inputs[i]).blur(() => {
+            for (let j = 0; j <= labels.length; j++) {
+                if (inputs[i].dataset.index == labels[j].dataset.index) {
+                    if (inputs[i].value == "") {
+                        $(labels[j]).css({
+                            "bottom": "5px",
+                            "font-size": "14px"
+                        })
+                    }
+                }
+            }
+        })
+    }
 
 
-  console.log("sxlsxlm")
 
-  
 });
 
-const Adjust = () => {
-  //Menu
-  var myContainerHeight = $("#my-container").outerHeight();
-  var chatHeadHeight =$("#menu .chat-header").outerHeight()+$("#search-section").outerHeight();
+const inputFocus = () => {
+    var inputs = document.getElementsByClassName("my-form-input");
+    var labels = document.getElementsByClassName("my-form-label");
+    for (let i = 0; i <= inputs.length; i++) {
+        for (let j = 0; j <= labels.length; j++) {
+            if (inputs[i].dataset.index == labels[j].dataset.index) {
+                if (inputs[i].value != "") {
+                    $(labels[j]).css({
+                        "bottom": "35px",
+                        "font-size": "12px"
+                    })
+                }
+            }
+        }
+    }
+}
 
-  console.log(chatHeadHeight); 
-  // Chat
-  var chatMessagesOutHeight = $("#chat-profile-info-wrapper").outerHeight()+$("#message-input-wrapper").outerHeight();
-  
-  $("#chat-list").css("height", myContainerHeight-chatHeadHeight+"px");
-  $("#chat-messages-container").css("height", myContainerHeight - chatMessagesOutHeight);
+const Adjust = () => {
+    //Menu
+    var myContainerHeight = $("#my-container").outerHeight();
+    var chatHeadHeight = $("#menu .chat-header").outerHeight() + $("#search-section").outerHeight();
+
+    console.log(chatHeadHeight);
+    // Chat
+    var chatMessagesOutHeight = $("#chat-profile-info-wrapper").outerHeight() + $("#message-input-wrapper").outerHeight();
+
+    $("#chat-list").css("height", myContainerHeight - chatHeadHeight + "px");
+    $("#chat-messages-container").css("height", myContainerHeight - chatMessagesOutHeight);
 };

c94dc6c1b1903a65a5d51ca2be145f8d7380e366
Nazaralieff <67953949+Nazaralieff@users.noreply.github.com>
2022-01-22 11:36:01 +0400

    BackGround image fixed

--- WhatsFakesApp-Asp.Net/wwwroot/assets/js/script.js
+++ WhatsFakesApp-Asp.Net/wwwroot/assets/js/script.js
@@ -62,18 +62,11 @@ const inputFocus = () => {
 const Adjust = () => {
     //Menu
     var myContainerHeight = $("#my-container").outerHeight();
-    console.log(myContainerHeight);
 
     var chatHeadHeight = $("#menu .chat-header").outerHeight() + $("#search-section").outerHeight();
-    console.log(chatHeadHeight);
     $("#chat-list").css("height", myContainerHeight - chatHeadHeight + "px");
-
 
     // Chat
     var chatMessagesOutHeight = $("#chat-profile-info-wrapper").outerHeight() + $("#message-input-wrapper").outerHeight();
-    //console.log(chatMessagesOutHeight);
-
-
-
     $("#chat-messages-container").css("height", myContainerHeight - chatMessagesOutHeight);
 };

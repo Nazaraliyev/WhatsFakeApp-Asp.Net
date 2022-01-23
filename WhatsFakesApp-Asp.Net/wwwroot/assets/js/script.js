88cd281eaf0f7779d7d0729699a0cdecabf7791c
Nazaralieff <67953949+Nazaralieff@users.noreply.github.com>
2022-01-23 14:25:45 +0400

    Chat scroll fixed with javascript

--- WhatsFakesApp-Asp.Net/wwwroot/assets/js/script.js
+++ WhatsFakesApp-Asp.Net/wwwroot/assets/js/script.js
@@ -1,10 +1,16 @@
 $(document).ready(() => {
     Adjust();
     //inputFocus();
-
     $(window).on("resize", () => {
         Adjust();
     });
+
+    
+
+
+    Message();
+
+    document.getElementById("send-message-btn").addEventListener("click", Message);
 
 
 
@@ -38,26 +44,7 @@ $(document).ready(() => {
         })
     }
 
-
-
 });
-
-const inputFocus = () => {
-    var inputs = document.getElementsByClassName("my-form-input");
-    var labels = document.getElementsByClassName("my-form-label");
-    for (let i = 0; i <= inputs.length; i++) {
-        for (let j = 0; j <= labels.length; j++) {
-            if (inputs[i].dataset.index == labels[j].dataset.index) {
-                if (inputs[i].value != "") {
-                    $(labels[j]).css({
-                        "bottom": "35px",
-                        "font-size": "12px"
-                    })
-                }
-            }
-        }
-    }
-}
 
 const Adjust = () => {
     //Menu
@@ -69,4 +56,23 @@ const Adjust = () => {
     // Chat
     var chatMessagesOutHeight = $("#chat-profile-info-wrapper").outerHeight() + $("#message-input-wrapper").outerHeight();
     $("#chat-messages-container").css("height", myContainerHeight - chatMessagesOutHeight);
+    $("#chat-home").css("height", myContainerHeight);
 };
+
+
+const Message = () => {
+    var container = $("#chat-messages-container").outerHeight();
+    var time = document.getElementsByClassName("chat-time");
+    var msg = document.getElementsByClassName("msg");
+    var msgsHeight = 0;
+    for (var i = 0; i < msg.length; i++) {
+        msgsHeight += ($(msg[i]).outerHeight() + 10);
+        if ((msgsHeight + $(time[0]).outerHeight()) >= container) {
+            document.getElementById("chat-messages-container").classList.remove("justify-content-end");
+            console.log("icerde");
+            return;
+        }
+    }
+
+    console.log("colde")
+}
